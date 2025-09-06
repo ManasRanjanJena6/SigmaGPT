dfa39ddbb74c55b19c0918a4a058bfde77ad5132
Manas Ranjan Jena <127410053+ManasRanjanJena6@users.noreply.github.com>
2025-09-07 02:18:36 +0530

    Update Sidebar.jsx

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext.jsx";
 import {v1 as uuidv1} from "uuid";
-import blackLogo from "../assets/blacklogo.png";
+import blackLogo from "./assets/blacklogo.png";
 
 function Sidebar() {
     const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
@@ -71,7 +71,7 @@ function Sidebar() {
     return (
         <section className="sidebar">
             <button onClick={createNewChat}>
-                <img src={blackLogo} alt="gpt logo" className="logo" />
+                <img src={blackLogo} alt="Logo" />
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
